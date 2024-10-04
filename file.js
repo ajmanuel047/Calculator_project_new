0cebd40ae5b76751aee8327803c684f39068ca90
Unuafe Ajiri <aj4short@rocketmail.com>
2024-10-04 03:22:19 +0100

    create the functions for each basic math operators and test them

--- file.js
+++ file.js
@@ -1,3 +1,19 @@
-let title = document.createElement('h1');
-title.textContent = 'Calculator Project';
-document.body.appendChild(title)
+function add(value1, value2){
+    return value1 + value2
+}
+console.log(add(3, 4))
+
+function subtract(value1, value2){
+    return value1 - value2
+}
+console.log(subtract(5, 3))
+
+function multiply(value1, value2){
+    return value1 * value2
+}
+console.log(multiply(4, 3))
+
+function divide(value1, value2){
+    return value1 / value2
+}
+console.log(divide(10, 2))

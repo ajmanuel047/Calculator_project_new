8bfa8c704754898ab3c5e99ddafa180099dae16d
Unuafe Ajiri <aj4short@rocketmail.com>
2024-10-05 23:54:55 +0100

    create variables for the DOM buttons

--- file.js
+++ file.js
@@ -23,3 +23,22 @@ function operate(operator, num1, num2){
     return add(num1, num2)
   }
   operate(add, 3, 4)
+
+const one = document.querySelector('#one');
+const two = document.querySelector('#two');
+const three = document.querySelector('#three');
+const four = document.querySelector('#four');
+const five = document.querySelector('#five');
+const six = document.querySelector('#six');
+const seven = document.querySelector('#seven');
+const eight = document.querySelector('#eight');
+const nine = document.querySelector('#nine');
+const clear = document.querySelector('#clear');
+const plus_minus = document.querySelector('#plus_minus');
+const percentage = document.querySelector('#percentage');
+const division = document.querySelector('#division');
+const multiplication = document.querySelector('#multiplication');
+const subtraction = document.querySelector('#subtraction');
+const addition = document.querySelector('#addition');
+const equal = document.querySelector('#equal');
+const back_space = document.querySelector('#back_space');

4c29a9b0c87b4271bfba88379fef29b3b84505cf
Unuafe Ajiri <aj4short@rocketmail.com>
2024-10-05 23:46:06 +0100

    create a function to operate a basic math and also create variables for its parameters

--- file.js
+++ file.js
@@ -1,19 +1,25 @@
+
+let num1;
+let num2;
+let operator;
+
 function add(value1, value2){
     return value1 + value2
 }
-console.log(add(3, 4))
 
 function subtract(value1, value2){
     return value1 - value2
 }
-console.log(subtract(5, 3))
 
 function multiply(value1, value2){
     return value1 * value2
 }
-console.log(multiply(4, 3))
 
 function divide(value1, value2){
     return value1 / value2
 }
-console.log(divide(10, 2))
+
+function operate(operator, num1, num2){
+    return add(num1, num2)
+  }
+  operate(add, 3, 4)
